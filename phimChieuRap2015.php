e465c87541e8c8e0b243c08fb0e2501d376c89bf
maint <maint_d7tmdt@epu.edu.vn>
2015-06-06 09:16:49 -0700

    Phim chiếu rạp 2015

---
+++ phimChieuRap2015.php
@@ -0,0 +1,111 @@
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+        <title>Phim chiếu rạp 2015</title>
+    </head>
+    <body>
+        <div class="container">
+            <?php include 'menu.php'; ?>
+            <div class="list col-md-8">
+                <div class="title col-md-12 col-sm-6">
+                    <a href="#" target="_self">XEM PHIM</a> > <a href="#">PHIM CHIẾU RẠP 2015</a>
+                </div>
+                <!--Title-->
+                <?php include 'formSearch.php'; ?>
+                <div class="block-body slider col-lg-12 col-md-12 col-sm-12 col-xs-12" style="margin-top: 3%">
+                    <div class="item col-lg-4 col-md-4 col-sm-6 owl-item" > 
+                        <div class="inner"> 
+                            <a class="poster" href="phim-bo/mat-dang-cuoc-doi_5790/" > 
+                                <img src="http://cdn.phim3s.net/images/films/thumb/mat-dang-cuoc-doi-the-women-of-our-home-2011.jpg" > 
+
+                            </a>  
+                            <span class="status">125/125</span>  
+                            <a class="name" href="phim-bo/mat-dang-cuoc-doi_5790/" >Mật Đắng Cuộc Đời</a> 
+                            <dfn>The Women of Our Home</dfn> 
+                            <dfn>2011</dfn> </div> 
+                    </div>
+                    <div class="item col-lg-4 col-md-4 col-sm-6  " > 
+                        <div class="inner">
+                            <a class="poster" href="phim-le/tu-than-trong-nha_5659/" >
+                                <img src="http://cdn.phim3s.net/images/films/thumb/tu-than-trong-nha-home-sweet-home-2013.jpg"> 
+
+                            </a>  
+                            <span class="status">Bản Đẹp</span>   
+                            <a class="name" href="phim-le/tu-than-trong-nha_5659/" >Tử Thần Trong Nhà</a> 
+                            <dfn>Home Sweet Home</dfn> 
+                            <dfn>2013</dfn> 
+                        </div> 
+                    </div>
+                    <div class="item col-lg-4 col-md-4 col-sm-6owl-item"> 
+                        <div class="inner"> 
+                            <a class="poster" href="phim-bo/dat-me_8200/" title="Đất Mẹ - Home land"> 
+                                <img src="http://cdn.phim3s.net/images/films/thumb/dat-me-home-land-2014.jpg" > 
+                            </a>  
+                            <span class="status">Tập 12</span>   
+                            <a class="name" href="phim-bo/dat-me_8200/" >Đất Mẹ</a> 
+                            <dfn>Home land</dfn> 
+                            <dfn>2014</dfn> 
+                        </div> 
+                    </div>   
+                </div> 
+                <div class="block-body slider col-lg-12 col-md-12 col-sm-12 col-xs-12" style="margin-top: 3%">
+                    <div class="item col-lg-4 col-md-4 col-sm-6 owl-item" > 
+                        <div class="inner"> 
+                            <a class="poster" href="phim-bo/mat-dang-cuoc-doi_5790/" > 
+                                <img src="http://cdn.phim3s.net/images/films/thumb/mat-dang-cuoc-doi-the-women-of-our-home-2011.jpg" > 
+
+                            </a>  
+                            <span class="status">125/125</span>  
+                            <a class="name" href="phim-bo/mat-dang-cuoc-doi_5790/" >Mật Đắng Cuộc Đời</a> 
+                            <dfn>The Women of Our Home</dfn> 
+                            <dfn>2011</dfn> </div> 
+                    </div>
+                    <div class="item col-lg-4 col-md-4 col-sm-6  " > 
+                        <div class="inner">
+                            <a class="poster" href="phim-le/tu-than-trong-nha_5659/" >
+                                <img src="http://cdn.phim3s.net/images/films/thumb/tu-than-trong-nha-home-sweet-home-2013.jpg"> 
+
+                            </a>  
+                            <span class="status">Bản Đẹp</span>   
+                            <a class="name" href="phim-le/tu-than-trong-nha_5659/" >Tử Thần Trong Nhà</a> 
+                            <dfn>Home Sweet Home</dfn> 
+                            <dfn>2013</dfn> 
+                        </div> 
+                    </div>
+                    <div class="item col-lg-4 col-md-4 col-sm-6  owl-item"> 
+                        <div class="inner"> 
+                            <a class="poster" href="phim-bo/dat-me_8200/" title="Đất Mẹ - Home land"> 
+                                <img src="http://cdn.phim3s.net/images/films/thumb/dat-me-home-land-2014.jpg" > 
+                            </a>  
+                            <span class="status">Tập 12</span>   
+                            <a class="name" href="phim-bo/dat-me_8200/" >Đất Mẹ</a> 
+                            <dfn>Home land</dfn> 
+                            <dfn>2014</dfn> 
+                        </div> 
+                    </div>   
+                </div> 
+                <div class="form col-lg-12">
+                    <ul class="pager">  
+                        <li><a href="#">Previous</a></li> 
+                        <li><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">Next</a></li>  
+
+                    </ul>  
+                </div><!--Phân Trang-->
+            </div>
+            <!--List film-->
+            <?php include 'advertise.php'; ?>
+            <?php include 'footer.php'; ?>
+        </div>
+         <script src="js/js.1.11.2.js" type="text/javascript"></script>
+        <script src="js/js.1.11.1.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    </body>
+</html>
+
