4d445efa3610666fb5b9bd06ae61e638873f07df
maint <maint_d7tmdt@epu.edu.vn>
2015-06-06 09:10:32 -0700

    trang phim chi tiết

---
+++ detail.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en"> 
+    <head>
+        <title>TrangChiTiet</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/styleDetail.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <div class="container">
+            <?php include 'menu.php'; ?>
+            <!-- Row main<div class="row main">-->
+            <!--left banner -->
+            <!--<div class="row left col-lg-2 col-md-2 col-sm-2 col-xs-2">
+                <div class="code col-lg-2 col-md-2 col-sm-2 col-xs-2">
+                    <iframe  style="border:none;overflow:hidden; height: 600px; margin-top: 107px;  " 
+                             src="http://b.adsquangcao.com//ads/index/94" >
+                    </iframe>
+                </div>
+            </div>-->
+
+            <!-- Main List Film -->
+            <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+                <div  class="adv01 col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <a href="//lg.logging.admicro.vn/adn?dmn=http%3A//phim3s.net/danh-sach/phim-bo/&amp;lsn=1433437201599&amp;cmpg=3030&amp;items=12876&amp;zid=12611&amp;cid=-1&amp;tp=11&amp;cov=1&amp;pr=1360672724&amp;re=http%3A%2F%2Fwww.lazada.vn%2Fmay-van-vit-dung-pin-makita-6723dw-48v-50671.html%3Fwt_dp_l%3Dvn.display_local.adx.%5Baa%5D_06000000000_MA703HL28WYXANVN-52322_vanvitdungpin_nonre980x90..%26utm_source%3Dadx%26utm_medium%3Ddisplay_local%26utm_campaign%3D%5Baa%5D_06000000000_MA703HL28WYXANVN-52322_vanvitdungpin_nonre980x90_%26utm_content%3Dphim3s.net&amp;adc_cpa=1" target="_blank">
+                        <img border="0"  src="//adi.vcmedia.vn/adt/adn/2015/06/980x9-ad1556dc5303b623.jpg" class="adv01 col-lg-12 col-md-12 col-sm-12 col-xs-12" alt="Responsive image"></a>
+                    <a class="admLogoAdx12611" href="http://adx.admicro.vn/?utm_source=Admicro&amp;utm_medium=phim3s.net&amp;utm_campaign=adxzone" target="_blank">
+
+                    </a>
+                </div>
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" >
+                    <object data="http://phim3s.net/ad/2015/Top_banner_exclusive.swf" 
+                            class="adv02 col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    </object>
+                </div>
+                <div  class=" col-lg-12 col-md-12 col-sm-12 col-xs-12" >
+                    <iframe src="http://ad.doubleclick.net/ddm/adi/N135802.1939700PHIM3S/B8709353.117738769;sz=980x120;ord=[timestamp]?" 
+                            style="border: none;" alt="Responsive image"  class="adv03 col-lg-12 col-md-12 col-sm-12 col-xs-12" >
+
+                    </iframe>
+                </div>
+                <div class=" col-lg-12 col-md-12 col-sm-12 col-xs-12"> 
+                    <div   itemtype="http://schema.org/Movie"> 
+                        <div class="row titleFilm col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <span style="margin-left: 5%; font-size: 120%">Xem Phim > Phim Lẻ > Hành Trình Trở Về</span>
+                        </div> 
+                        <div class="row"> 
+
+                            <div class="col1 col-lg-4 col-md-4 col-sm-8 col-xs-10" style="margin-top: 3%;"> 
+                                <div class="poster">  
+                                    <span class="status">Bản Đẹp</span>  
+                                    <img src="http://cdn.phim3s.net/images/films/thumb/hanh-trinh-tro-ve-home-2015.jpg"> </div>  
+                                <div class="trailer"> 
+                                    <a target="_blank" href="#" 
+                                       class="btn btn-success" style="margin-top: 2%; margin-left: 20%">
+                                        Xem trailer 
+                                    </a> 
+                                </div>  
+                            </div>  
+                            <div class="col2 col-lg-8 col-md-8"> 
+                                <div class="name block-title style2"> 
+                                    <h2>Hành Trình Trở Về</h2> 
+                                    <hr />
+                                </div> 
+                                <div class="name2"> 
+                                    <dfn>Home (2015)</dfn> 
+                                </div> 
+                                <dl>
+                                    <dt>Thể loại:</dt> 
+                                    <dd>  
+                                        <a  href="#">Phim Hoạt Hình</a>,   
+                                        <a  href="#">Phim Phiêu Lưu</a>  
+                                    </dd> 
+                                    <dt>Quốc gia:</dt> 
+                                    <dd>
+                                        <a title="Phim Mỹ" href="#">Mỹ</a>
+                                    </dd> 
+                                    <dt>Đạo diễn:</dt> 
+                                    <dd>Tim Johnson</dd> 
+                                    <dt>Diễn viên:</dt> 
+                                    <dd>Jim Parsons, Rihanna, Steve Martin</dd> 
+                                    <dt>Thời lượng:</dt> 
+                                    <dd>94 Phút</dd> 
+                                    <dt>Năm phát hành:</dt> 
+                                    <dd>2015</dd>   
+                                    <hr/>
+                                </dl> 
+                                <div class="extra-info"> 
+                                    <div class="views col-lg-6 col-md-6 col-sm-6 col-xs-6"> 
+                                        <i class="micon views">
+                                            <img src="img/eye.png" style="width: 6%; height: 3%"/>
+                                        </i> 
+                                        <span>169,870</span> 
+                                    </div> <div class="like col-lg-6 col-md-6 col-sm-6 col-xs-6"> 
+                                        <i class="micon heart">
+                                            <img src="img/icon-heart.png" style="width: 6%; height: 3%"/>
+                                        </i> 
+                                        <span>10 / 4 lượt</span> 
+                                    </div>
+                                    <hr />
+                                </div> 
+
+                                <div class="buttons row"> 
+                                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">  
+                                        <div class="watch"> 
+                                            <a href="xemPhim.php" title="Hành Trình Trở Về">
+                                                <i class="btn btn-success col-lg-6 col-md-6 col-sm-6 col-xs-6">Xem phim</i>
+
+                                            </a> 
+                                        </div>          
+                                    </div>
+                                </div> 
+                                <hr />
+                            </div> 
+                        </div> 
+                    </div>
+                    <!--/.block--> 
+                    <div class="btn btn-info col-lg-12 col-md-12 col-sm-12 col-xs-12"><h4 style="font-size: 150%">Thông tin phim</h4></div> 
+                    <div > 
+                        <div>
+                            <strong style="font-size: 120%">Hành Trình Trở Về</strong>
+                        </div> 
+                        <div>
+                            <p>Bộ <a href="#" title="phim">
+                                    <strong>phim</strong>
+                                </a> 
+                                là hành trình vui nhộn giữa Oh, một người ngoài hành tinh đáng yêu đang 
+                                chạy trốn khỏi chủng tộc của mình, và Tip, một cô bé đầy sức sống, cùng 
+                                con mèo cưng của cô. Trên chuyến đi ấy, họ đã thấu hiểu về nhau hơn, xây 
+                                dựng một tình bạn khắng khít, và cùng cứu lấy thế giới.
+                            </p>
+                        </div> 
+                    </div> 
+                    <!--.block-->  
+                    <?php include 'phimLienQuan.php'; ?>
+                    <!--/.block--> 
+                </div>
+            </div>
+            <?php include 'advertise.php'; ?>
+            
+            <!-- right main-->
+            <!--<div class="row right col-lg-2 col-md-2 col-sm-2 col-xs-2  " style="margin-top: 107px">
+                <div class="code col-lg-2 col-md-2 col-sm-2 col-xs-2">
+                    <iframe width="120px" height="600px" style="border:none;overflow:hidden; margin-left: 70px ; ;" 
+                            src="http://b.adsquangcao.com//ads/index/94">
+                    </iframe>
+                </div>
+            </div>-->
+
+            <!-- end row main  </div> -->  
+            <?php include 'footer.php'; ?>
+        </div>
+        
+        <script src="js/js.1.11.2.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    </body>
+</html>
+
