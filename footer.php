025614be18369983434da1eeb7108a0cbb4ec6d1
maint <maint_d7tmdt@epu.edu.vn>
2015-06-06 01:15:24 -0700

    footer

---
+++ footer.php
@@ -0,0 +1,47 @@
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <div class="footer col-md-12 col-sm-6">
+            <div class="des col-md-8">
+                <p>
+                    Xem phim online miễn phí, chất lượng hình ảnh rõ nét, tốc độ tải phim nhanh,
+                    xem phim không phải chờ đợi lâu. Phim 3S luôn cập nhật phim mới để mang đến 
+                    cho các bạn những bộ phim hành động, võ thuật, phim chiếu rạp, các thể loại
+                    phim tâm lý, tình cảm cực lôi cuốn và hấp dẫn nhất. Đặc biệt website rất thân 
+                    thiện với người dùng và hạn chế tối đa các quảng cáo gây khó chịu khi xem phim. 
+                    Chúc các bạn xem phim vui vẻ.
+                </p>
+            </div> 
+            <div class="connect col-md-2">
+                <span style="color: #31b0d5;">Liên hệ</span>
+                <ul>
+                    <li>
+                        <a href="#">Liên hệ quảng cáo</a>
+                    </li>
+                    <li>
+                        <a href="#">Đăng tải phim</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="connect col-md-2">
+                <span style="color: #31b0d5;">Điều khoản sử dụng</span>
+                <ul>
+                    <li>
+                        <a href="#">Điều khoản chung</a>
+                        
+                    </li>
+                    <li><a href="#">Bản quyền và trách nhiệm nội dung</a></li>
+                </ul>
+            </div>
+        </div>
+        <script src="js/js.1.11.2.js" type="text/javascript"></script>
+        <script src="js/js.1.11.1.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    </body>
+</html>
