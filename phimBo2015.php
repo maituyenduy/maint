aec0cc817d3873faef188d78d248b6a54e6b2603
maint <maint_d7tmdt@epu.edu.vn>
2015-06-06 01:45:32 -0700

    phim bộ 2015

---
+++ phimBo2015.php
@@ -0,0 +1,231 @@
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+        <title>Phim bộ 2015</title>
+    </head>
+    <body>
+        <div class="container">
+            <?php include 'menu.php'; ?>
+            <div class="list col-md-8">
+                <div class="title col-md-12 col-sm-6">
+                    <a href="#" target="_self">XEM PHIM</a> > <a href="#">PHIM Bộ 2015</a>
+                </div>
+                <!--Title-->
+                <?php include 'formSearch.php'; ?>
+                <div class="item col-lg-3 col-md-4 col-sm-6">
+                    <div class="inner">
+                        <a class="poster" title="Mặt Nạ - Mask" href="#">
+                            <img src="img/mat-na-mask-2015.jpg" alt="Mặt Nạ - Mask" style="width: 150px; height: 200px"/>
+                            <i class="hidden-xs hidden-sm"></i>
+                        </a>
+                        <span class="status">Tập 02</span><br />
+                        <a class="name" title="Mặt Nạ - Mask" href="phim-bo/mat-na_8469/">Mặt Nạ</a><br />
+                        <dfn>Mask</dfn><br />
+                        <dfn>2015</dfn>
+                    </div>
+                </div><!--code film-->
+                <div class="item col-lg-3 col-md-4 col-sm-6">
+                    <div class="inner">
+                        <a class="poster" title="Người máy trỗi dậy" href="#">
+                            <img src="img/nguoi-may-troi-day-ex-machina-2015.jpg" alt="Người máy trỗi dậy" style="width: 150px; height: 200px"/>
+                            <i class="hidden-xs hidden-sm"></i>
+                        </a>
+                        <span class="status">Bản đẹp</span><br />
+                        <a class="name" title="Người máy trỗi dậy" href="#">Người máy trỗi dậy</a><br />
+                        <dfn>Ex Machina</dfn><br />
+                        <dfn>2015</dfn>
+                    </div>
+                </div><!--code film-->
+                <div class="item col-lg-3 col-md-4 col-sm-6">
+                    <div class="inner">
+                        <a class="poster" title="Hành trình trở về nhà - Home" href="phim-bo/mat-na_8469/">
+                            <img src="img/hanh-trinh-tro-ve-home-2015.jpg" alt="Hành trình trở về nhà - Home" style="width: 150px; height: 200px"/>
+                            <i class="hidden-xs hidden-sm"></i>
+                        </a>
+                        <span class="status">Bản Đẹp</span><br />
+                        <a class="name" title="Hành trình trở về nhà - Home" href="#">Hành trình trở về nhà</a><br />
+                        <dfn>Home</dfn><br />
+                        <dfn>2015</dfn>
+                    </div>
+                </div><!--code film-->
+                <div class="item col-lg-3 col-md-4 col-sm-6">
+                    <div class="inner">
+                        <a class="poster" title="Khi tôi 20 - Twenty" href="phim-bo/mat-na_8469/">
+                            <img src="img/khi-toi-20-twenty-2015.jpg" alt="Khi tôi 20 - Twenty" style="width: 150px; height: 200px"/>
+                            <i class="hidden-xs hidden-sm"></i>
+                        </a>
+                        <span class="status">HDRIP</span><br />
+                        <a class="name" title="Mặt Nạ - Mask" href="phim-bo/mat-na_8469/">Khi tôi 20</a><br />
+                        <dfn>Twenty</dfn><br />
+                        <dfn>2015</dfn>
+                    </div>
+                </div><!--code film-->
+                <div class="item col-lg-3 col-md-4 col-sm-6">
+                    <div class="inner">
+                        <a class="poster" title="Mặt Nạ - Mask" href="#">
+                            <img src="img/mat-na-mask-2015.jpg" alt="Mặt Nạ - Mask" style="width: 150px; height: 200px"/>
+                            <i class="hidden-xs hidden-sm"></i>
+                        </a>
+                        <span class="status">Tập 02</span><br />
+                        <a class="name" title="Mặt Nạ - Mask" href="phim-bo/mat-na_8469/">Mặt Nạ</a><br />
+                        <dfn>Mask</dfn><br />
+                        <dfn>2015</dfn>
+                    </div>
+                </div><!--code film-->
+                <div class="item col-lg-3 col-md-4 col-sm-6">
+                    <div class="inner">
+                        <a class="poster" title="Người máy trỗi dậy" href="#">
+                            <img src="img/nguoi-may-troi-day-ex-machina-2015.jpg" alt="Người máy trỗi dậy" style="width: 150px; height: 200px"/>
+                            <i class="hidden-xs hidden-sm"></i>
+                        </a>
+                        <span class="status">Bản đẹp</span><br />
+                        <a class="name" title="Người máy trỗi dậy" href="#">Người máy trỗi dậy</a><br />
+                        <dfn>Ex Machina</dfn><br />
+                        <dfn>2015</dfn>
+                    </div>
+                </div><!--code film-->
+                <div class="item col-lg-3 col-md-4 col-sm-6">
+                    <div class="inner">
+                        <a class="poster" title="Hành trình trở về nhà - Home" href="phim-bo/mat-na_8469/">
+                            <img src="img/hanh-trinh-tro-ve-home-2015.jpg" alt="Hành trình trở về nhà - Home" style="width: 150px; height: 200px"/>
+                            <i class="hidden-xs hidden-sm"></i>
+                        </a>
+                        <span class="status">Bản Đẹp</span><br />
+                        <a class="name" title="Hành trình trở về nhà - Home" href="#">Hành trình trở về nhà</a><br />
+                        <dfn>Home</dfn><br />
+                        <dfn>2015</dfn>
+                    </div>
+                </div><!--code film-->
+                <div class="item col-lg-3 col-md-4 col-sm-6">
+                    <div class="inner">
+                        <a class="poster" title="Khi tôi 20 - Twenty" href="phim-bo/mat-na_8469/">
+                            <img src="img/khi-toi-20-twenty-2015.jpg" alt="Khi tôi 20 - Twenty" style="width: 150px; height: 200px"/>
+                            <i class="hidden-xs hidden-sm"></i>
+                        </a>
+                        <span class="status">HDRIP</span><br />
+                        <a class="name" title="Mặt Nạ - Mask" href="phim-bo/mat-na_8469/">Khi tôi 20</a><br />
+                        <dfn>Twenty</dfn><br />
+                        <dfn>2015</dfn>
+                    </div>
+                </div><!--code film-->
+                <div class="item col-lg-3 col-md-4 col-sm-6">
+                    <div class="inner">
+                        <a class="poster" title="Mặt Nạ - Mask" href="#">
+                            <img src="img/mat-na-mask-2015.jpg" alt="Mặt Nạ - Mask" style="width: 150px; height: 200px"/>
+                            <i class="hidden-xs hidden-sm"></i>
+                        </a>
+                        <span class="status">Tập 02</span><br />
+                        <a class="name" title="Mặt Nạ - Mask" href="phim-bo/mat-na_8469/">Mặt Nạ</a><br />
+                        <dfn>Mask</dfn><br />
+                        <dfn>2015</dfn>
+                    </div>
+                </div><!--code film-->
+                <div class="item col-lg-3 col-md-4 col-sm-6">
+                    <div class="inner">
+                        <a class="poster" title="Người máy trỗi dậy" href="#">
+                            <img src="img/nguoi-may-troi-day-ex-machina-2015.jpg" alt="Người máy trỗi dậy" style="width: 150px; height: 200px"/>
+                            <i class="hidden-xs hidden-sm"></i>
+                        </a>
+                        <span class="status">Bản đẹp</span><br />
+                        <a class="name" title="Người máy trỗi dậy" href="#">Người máy trỗi dậy</a><br />
+                        <dfn>Ex Machina</dfn><br />
+                        <dfn>2015</dfn>
+                    </div>
+                </div><!--code film-->
+                <div class="item col-lg-3 col-md-4 col-sm-6">
+                    <div class="inner">
+                        <a class="poster" title="Hành trình trở về nhà - Home" href="phim-bo/mat-na_8469/">
+                            <img src="img/hanh-trinh-tro-ve-home-2015.jpg" alt="Hành trình trở về nhà - Home" style="width: 150px; height: 200px"/>
+                            <i class="hidden-xs hidden-sm"></i>
+                        </a>
+                        <span class="status">Bản Đẹp</span><br />
+                        <a class="name" title="Hành trình trở về nhà - Home" href="#">Hành trình trở về nhà</a><br />
+                        <dfn>Home</dfn><br />
+                        <dfn>2015</dfn>
+                    </div>
+                </div><!--code film-->
+                <div class="item col-lg-3 col-md-4 col-sm-6">
+                    <div class="inner">
+                        <a class="poster" title="Khi tôi 20 - Twenty" href="phim-bo/mat-na_8469/">
+                            <img src="img/khi-toi-20-twenty-2015.jpg" alt="Khi tôi 20 - Twenty" style="width: 150px; height: 200px"/>
+                            <i class="hidden-xs hidden-sm"></i>
+                        </a>
+                        <span class="status">HDRIP</span><br />
+                        <a class="name" title="Mặt Nạ - Mask" href="phim-bo/mat-na_8469/">Khi tôi 20</a><br />
+                        <dfn>Twenty</dfn><br />
+                        <dfn>2015</dfn>
+                    </div>
+                </div><!--code film-->
+                <div class="item col-lg-3 col-md-4 col-sm-6">
+                    <div class="inner">
+                        <a class="poster" title="Mặt Nạ - Mask" href="#">
+                            <img src="img/mat-na-mask-2015.jpg" alt="Mặt Nạ - Mask" style="width: 150px; height: 200px"/>
+                            <i class="hidden-xs hidden-sm"></i>
+                        </a>
+                        <span class="status">Tập 02</span><br />
+                        <a class="name" title="Mặt Nạ - Mask" href="phim-bo/mat-na_8469/">Mặt Nạ</a><br />
+                        <dfn>Mask</dfn><br />
+                        <dfn>2015</dfn>
+                    </div>
+                </div><!--code film-->
+                <div class="item col-lg-3 col-md-4 col-sm-6">
+                    <div class="inner">
+                        <a class="poster" title="Người máy trỗi dậy" href="#">
+                            <img src="img/nguoi-may-troi-day-ex-machina-2015.jpg" alt="Người máy trỗi dậy" style="width: 150px; height: 200px"/>
+                            <i class="hidden-xs hidden-sm"></i>
+                        </a>
+                        <span class="status">Bản đẹp</span><br />
+                        <a class="name" title="Người máy trỗi dậy" href="#">Người máy trỗi dậy</a><br />
+                        <dfn>Ex Machina</dfn><br />
+                        <dfn>2015</dfn>
+                    </div>
+                </div><!--code film-->
+                <div class="item col-lg-3 col-md-4 col-sm-6">
+                    <div class="inner">
+                        <a class="poster" title="Hành trình trở về nhà - Home" href="phim-bo/mat-na_8469/">
+                            <img src="img/hanh-trinh-tro-ve-home-2015.jpg" alt="Hành trình trở về nhà - Home" style="width: 150px; height: 200px"/>
+                            <i class="hidden-xs hidden-sm"></i>
+                        </a>
+                        <span class="status">Bản Đẹp</span><br />
+                        <a class="name" title="Hành trình trở về nhà - Home" href="#">Hành trình trở về nhà</a><br />
+                        <dfn>Home</dfn><br />
+                        <dfn>2015</dfn>
+                    </div>
+                </div><!--code film-->
+                <div class="item col-lg-3 col-md-4 col-sm-6">
+                    <div class="inner">
+                        <a class="poster" title="Khi tôi 20 - Twenty" href="phim-bo/mat-na_8469/">
+                            <img src="img/khi-toi-20-twenty-2015.jpg" alt="Khi tôi 20 - Twenty" style="width: 150px; height: 200px"/>
+                            <i class="hidden-xs hidden-sm"></i>
+                        </a>
+                        <span class="status">HDRIP</span><br />
+                        <a class="name" title="Mặt Nạ - Mask" href="phim-bo/mat-na_8469/">Khi tôi 20</a><br />
+                        <dfn>Twenty</dfn><br />
+                        <dfn>2015</dfn>
+                    </div>
+                </div><!--code film-->
+                <div class="form col-lg-12">
+                    <ul class="pager">  
+                        <li><a href="#">Previous</a></li> 
+                        <li><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">Next</a></li>  
+
+                    </ul>  
+                </div><!--Phân Trang-->
+            </div>
+            <!--List film-->
+            <?php include 'advertise.php'; ?>
+            <?php include 'footer.php'; ?>
+        </div>
+         <script src="js/js.1.11.2.js" type="text/javascript"></script>
+        <script src="js/js.1.11.1.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    </body>
+</html>
+
