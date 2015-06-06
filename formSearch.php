225c1882d79dab952281c9b7e1c8637a11f76694
maint <maint_d7tmdt@epu.edu.vn>
2015-06-06 01:17:44 -0700

    form tìm kiếm

---
+++ formSearch.php
@@ -0,0 +1,30 @@
+<div class="form col-md-12">
+    <div class="btn-group">  
+        <div class="btn-group"> 
+            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">  
+                -:- Thể loại -:- <span class="caret"></span></button>  
+            <ul class="dropdown-menu" role="menu">  
+                <li><a href="#">Hành động</a></li>  
+                <li><a href="#">Tâm lý</a></li>  
+            </ul>  
+        </div>  
+        <div class="btn-group"> 
+            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">  
+                -:- Quốc gia -:- <span class="caret"></span></button>  
+            <ul class="dropdown-menu" role="menu">  
+                <li><a href="#">Mỹ</a></li>  
+                <li><a href="#">Thái Lan</a></li>  
+            </ul>  
+        </div>
+        <div class="btn-group"> 
+            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">  
+                -:- Năm -:- <span class="caret"></span></button>  
+            <ul class="dropdown-menu" role="menu">  
+                <li><a href="#">2015</a></li>  
+                <li><a href="#">2014</a></li>  
+            </ul>  
+        </div>
+        <button type="button" class="btn btn-info">Lọc phim</button> 
+    </div>
+    
+</div>
